ea4b8d41c729c58851a7d2efc901d22db3dfa997
cgluttappi <95345813+cgluttappi@users.noreply.github.com>
2021-12-30 12:49:58 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
     var respoimage = await axios.get(config.LIZA, { responseType: 'arraybuffer' })
 
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , thumbnail: fs.readFileSync('20210811_082543.jpg'), mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Links ☟︎︎︎≈≈≈≈≈≈≈≈*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , thumbnail: fs.readFileSync('KILLADI.jpg'), mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Links ☟︎︎︎≈≈≈≈≈≈≈≈*
  
 *owner number wa.me/916282137207*
    
@@ -24,11 +24,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
 *githublink       https://github.com/cgluttappi/LIZA*
 
 
-*audio commads    _https://github.com/cgluttappi/LIZA_*
-
-*Bot Make Video ytube https://github.com/cgluttappi/LIZA-_*
-
-*sticker commads  _https://github.com/cgluttappi/LIZA_*     
+*     
 `}) 
 
 })); 
